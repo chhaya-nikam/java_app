a5b3e626cf430eaab7a9827e522431161ee38e76
chhaya-nikam <chhaya.vikrant.nikam@pwc.com>
2025-08-05 15:19:58 +0530

    Add initial implementation of App class with main method and greet function

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+
+    public void greet() {
+        System.out.println("Greetings from the App class!");
+    }
+}
